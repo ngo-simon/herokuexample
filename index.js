82d70feae3d74ad5d0841fdd255b2d740e013db0
ngo-simon <ngo.simon97@gmail.com>
2020-01-10 22:07:05 -0500

    uibuild

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
   
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
@@ -36,7 +36,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/notes', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
@@ -57,7 +57,7 @@ app.post('/notes', (request, response) => {
   response.json(note)
 })
 
-app.get('/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
   if (note) {
@@ -70,7 +70,7 @@ app.get('/notes/:id', (request, response) => {
   response.json(note)
 })
 
-app.delete('/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
